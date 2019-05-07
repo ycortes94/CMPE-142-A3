5ff9cb17449ee20d7cdbfc5e420f894502f3de22
Yosimy Cortes <ycortes94@yahoo.com>
2019-05-07 16:14:37 -0700

    added switch case table for ACTION table

--- memManagement.c
+++ memManagement.c
@@ -4,13 +4,57 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void FIFO()
+{
+    
+}
+
+void LRU()
+{
+   
+}
+
+void Random()
+{
+
+
+}
+
 int main(int argc, char **argv)
 {
    static const char filename[] = "memory.dat";
    FILE *fp = fopen(filename, "r");
    if (fp != NULL)
    {
-      
+      char ch='b';
+     //ACTION table
+     switch (ch)
+     {
+         case 'C':
+            printf("Process is Created");
+            break;
+         case 'T':
+            printf("Process is Terminated");
+            break;
+         case 'A':
+            printf("Process allocated memory at address 'PAGE'");
+            break;
+         case 'R':
+            printf("Process is read");
+            break;
+         case 'W':
+            printf("Process wrote to 'PAGE'");
+            break;
+         case 'F':
+            printf("Process freed memory at address 'PAGE'");
+            break;
+         default:
+            printf("Default ");
+    }
+
+      FIFO();
+      LRU();
+      Random();
    }
    else
    {
