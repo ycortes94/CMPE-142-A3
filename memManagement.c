eaa2afc01b59eba5dc05c3f7c02846174dbaea8e
ycortes94 <ycortes94@yahoo.com>
2019-05-14 23:39:00 -0700

    Update memManagement.c

--- memManagement.c
+++ memManagement.c
@@ -13,28 +13,28 @@
 
 int FIFO(int pages[], int n, int capacity)
 {
-   //    HashSet<int> s = new HashSet<int>(capacity);
-   //    Queue indexes = new Queue();
-   //    int page_faults = 0;
-   //    for(int i = 0; i < n; i++) {
-   //       if(s.Count < capacity) {
-   //          if(!s.Contains(pages[i])) {
-   //             s.Add(pages[i]);
-   //             page_faults++;
-   //             indexes.Enqueue(pages[i]);
-   //          }
-   //       } else {
-   //          if(!s.Contains(pages[i])) {
-   //             int val = (int)indexes.Peek();
-   //             indexes.Dequeue();
-   //             s.Removal(val);
-   //             s.Add(pages[i]);
-   //             indexes.Enqueue(pages[i]);
-   //             page_faults++;
-   //          }
-   //       }
-   //    }
-   // return page_faults;
+       HashSet<int> s = new HashSet<int>(capacity);
+       Queue indexes = new Queue();
+       int page_faults = 0;
+       for(int i = 0; i < n; i++) {
+          if(s.Count < capacity) {
+             if(!s.Contains(pages[i])) {
+                s.Add(pages[i]);
+                page_faults++;
+                indexes.Enqueue(pages[i]);
+             }
+          } else {
+             if(!s.Contains(pages[i])) {
+                int val = (int)indexes.Peek();
+                indexes.Dequeue();
+                s.Removal(val);
+                s.Add(pages[i]);
+                indexes.Enqueue(pages[i]);
+                page_faults++;
+             }
+          }
+       }
+    return page_faults;
 }
 
 /*FOR LEAST RECENTLY USED ALGORITHM*/
