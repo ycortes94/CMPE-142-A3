25673971a5ba24528dc2224a2229ad008d443164
ycortes94 <ycortes94@yahoo.com>
2019-05-14 23:39:20 -0700

    Update memManagement.c

--- memManagement.c
+++ memManagement.c
@@ -259,7 +259,7 @@ int main(int argc, char **argv)
    {
       int frame, debug;
       char *tfile;
-      // FIFO();
+      FIFO();
       LRU();
       Random(frame, tfile, debug);
       fclose(fp);
